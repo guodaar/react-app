12b98a6b7d50988fa114cdca71f03e2f1dcaba6b
Guoda Arlauskaite <guoda.arlauskaite@gmail.com>
2023-02-01 20:04:06 +0200

    fix routes

--- src/routes/const.js
+++ src/routes/const.js
@@ -1,16 +1,20 @@
+import MainLayout from "../layouts/MainLayout";
 import Home from "../pages/Home/Home";
 import Products from "../pages/Products/Products";
 
 export const HOME_PATH = "/";
 export const PRODUCTS_PATH = `${HOME_PATH}:category`;
 
-export const routes = [
-  {
-    path: HOME_PATH,
-    Component: Home,
-  },
-  {
-    path: PRODUCTS_PATH,
-    Component: Products,
-  },
-];
+export const mainLayoutRoutes = {
+  Layout: MainLayout,
+  routes: [
+    {
+      path: HOME_PATH,
+      Component: Home,
+    },
+    {
+      path: PRODUCTS_PATH,
+      Component: Products,
+    },
+  ],
+};
